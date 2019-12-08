1b1b9fb3b0e14593b8312005fec4d04998d6d28b
tomzacchia <tomzacchia@live.com>
2019-12-08 13:59:41 -0500

    Added get request for TOURS

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-// app.httpMethod
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from the server side',
-    app: 'Natours'
-  });
-});
+// Caching data via blocking code
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.send('Post req received');
+// Callbacks in express are called Route Handlers
+app.get('/api/v1/tours', (req, res) => {
+  // formatting data according to JSEND specs
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 
 const port = 3000;
