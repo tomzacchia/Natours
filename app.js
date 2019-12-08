855c84d84f6e7075c184c34a6e4e8c22eb09fb43
tomzacchia <tomzacchia@live.com>
2019-12-08 14:20:33 -0500

    Add post req handler to create new tour

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+// include .json() middleware, add data to body of req
+app.use(express.json());
+
 // Caching data via blocking code
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -18,6 +21,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { ...req.body, id: newId };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    // 201: created new resource
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const port = 3000;
