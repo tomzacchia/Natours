3b7155d434baea56b7532b9674c35e55b3ec475d
tomzacchia <tomzacchia@live.com>
2019-12-08 00:23:55 -0500

    Add package.json config and initial set up of server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+// app.httpMethod
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello from the server side',
+    app: 'Natours'
+  });
+});
+
+app.post('/', (req, res) => {
+  res.send('Post req received');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port: ${port}`);
+});
