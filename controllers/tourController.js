33f273df3385de13d366d5493cc23e7ae78b6dab
tomzacchia <tomzacchia@live.com>
2020-01-08 23:41:22 -0500

    Refactored mongoose deletTour handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,13 +39,25 @@ exports.getTourByID = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'TOUR UPDATED'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    // new option: returns modified document instead of original
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      data: {
+        updatedTour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail',
+      message: err
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -70,9 +82,17 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
