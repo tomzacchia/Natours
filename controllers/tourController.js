1ab73f480530387a7a238b30ba10d8ff93fd4598
tomzacchia <tomzacchia@live.com>
2020-01-09 22:04:28 -0500

    Removed comment from createTour handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -48,6 +48,7 @@ exports.updateTour = async (req, res) => {
     });
 
     res.status(200).json({
+      status: 'success',
       data: {
         updatedTour
       }
@@ -62,10 +63,6 @@ exports.updateTour = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    // const newTour = new Tour()
-    // newTour.save()
-
-    // Calling method on Tour model directly
     const newTour = await Tour.create(req.body);
 
     res.status(200).json({
