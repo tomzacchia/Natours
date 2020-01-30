5ca5933f2bc2179d0fd6bbd40f101c5015e5965c
tomzacchia <tomzacchia@live.com>
2020-01-30 12:22:13 +0700

    Added protection against noSQL injection and xss

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +29,13 @@ app.use('/api', limiter);
 
 // BODY PARSER - INSERT BODY INTO REQ.BODY
 app.use(express.json({ limit: '10kb' }));
+
+// DATA SANITIZATION against noSQL query injection
+// removes operators from request
+app.use(mongoSanitize());
+
+// DATA SANITIZATION against XSS
+app.use(xss());
 
 // SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
